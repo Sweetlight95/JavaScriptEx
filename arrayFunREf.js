773b79dc90b6266a6f5f8e65111a0d2ef341c2ad
Sweetlight95 <princessadesuwa93@gmail.com>
2021-11-15 01:53:10 +0100

    up

---
+++ arrayFunREf.js
@@ -0,0 +1,6 @@
+const number = [2, 5, 10];
+const doubleArray = number.map((num) => {
+    return num * 2 
+});
+console.log(number);
+console.log(doubleArray);
---
+++ destructuring.js
@@ -0,0 +1,3 @@
+const number = [1, 5, 7, 4, 2];
+[num1,  ,num2] = number;
+console.log(num1, num2);
